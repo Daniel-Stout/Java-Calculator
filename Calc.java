8103073e0b204574ccd2c6c912ff2796e2f95267
Daniel-Stout <45444420+Daniel-Stout@users.noreply.github.com>
2019-06-13 09:56:16 -0500

    Add files via upload

--- Calc.java
+++ Calc.java
@@ -2,48 +2,56 @@
  *  Program Name : Simple Java Calculator
  *  Author : Daniel Stout II
  *  Date Created : December 17, 2018
+ *  Last Updated : June 13, 2019
  *  Description : This is a very simple Java Calculator  
  *  that runs in the Command Prompt / Terminal
 */      
 
 import java.util.Scanner;
 
+import static java.lang.System.*;
+
 public class Calc {
-    public static void main(String [] args) {
+    private static Scanner o;
+	private static Scanner input;
+	private static Scanner input2;
+	private static Scanner c;
+
+	public static void main(String [] args) {
         runCalc(true);
     }
 
     private static void add(double n1, double n2) {
         double res = n1 + n2;
-        System.out.println("\nResult : " + res);
+        out.println("\nResult : " + res);
         restart();
     }
     
     private static void sub(double n1, double n2) {
         double res = n1 - n2;
-        System.out.println("Result : " + res);
+        out.println("Result : " + res);
         restart();
     }
     
     private static void mult(double n1, double n2) {
         double res = n1 * n2;
-        System.out.println("\nResult : " + res);
+        out.println("\nResult : " + res);
         restart();
     } 
     
     private static void div(double n1, double n2) {
         if (n2 == 0) {
-            System.out.println("Error : Divide By Zero");
+            out.println("Error : Divide By Zero");
         } else {
             double res = n1 / n2;
-            System.out.println("\nResult : " + res);
+            out.println("\nResult : " + res);
         }
         restart();
     }
     
     private static void mod(double n1, double n2) {
         double res = n1 % n2;
-        System.out.println("\nResult : " + res);
+        out.println("\nResult : " + res);
         restart();
     }
 
@@ -52,11 +60,11 @@ public class Calc {
     }
 
     private static void restart() {
-        System.out.println("\nWould you like to do another?");
-        System.out.println("1. Yes");
-        System.out.println("2. No");
-        System.out.print("\nChoice : ");
-        Scanner c = new Scanner(System.in);
+        out.println("\nWould you like to do another?");
+        out.println("1. Yes");
+        out.println("2. No");
+        out.print("\nChoice : ");
+        c = new Scanner(in);
         int yesNo = c.nextInt();
 
         if (yesNo == 2){
@@ -75,24 +83,25 @@ public class Calc {
             end();
         }
         
-        System.out.println("\n1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("3. Multiplication");
-        System.out.println("4. Division");
-        System.out.println("5. Modulus");
+        out.println("Please enter the operator's corresponding number.");
+        out.println("\n1. Addition");
+        out.println("2. Subtraction");
+        out.println("3. Multiplication");
+        out.println("4. Division");
+        out.println("5. Modulus");
         
         System.out.print("\nOperation : ");
-        Scanner o = new Scanner(System.in);
+        o = new Scanner(in);
         int z = o.nextInt();
         int opp = z;
         
         System.out.print("\nFirst Number : ");
-        Scanner input = new Scanner(System.in);
+        input = new Scanner(in);
         double x = input.nextDouble();
         double n1 = x;
 
         System.out.print("Second Number : ");
-        Scanner input2 = new Scanner(System.in);
+        input2 = new Scanner(in);
         double y = input2.nextDouble();
         double n2 = y;
 
@@ -107,9 +116,9 @@ public class Calc {
         } if (opp == 5) {
             mod(n1, n2);
         } if (opp >= 6) {
-            System.out.println("That is not a correct opperation.");
+            out.println("That is not a correct opperation.");
         } if (opp <= 0) {
-            System.out.println("That is not a correct opperation.");
+            out.println("That is not a correct opperation.");
         }
     }
 }
